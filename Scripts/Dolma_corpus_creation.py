39fcc72e50d66e3cff331ec370ef8950f8876137
znhoughton <znhoughton@gmail.com>
2024-05-31 23:17:02 -0700

    updated corpus creation script

--- Scripts/Dolma_corpus_creation.py
+++ Scripts/Dolma_corpus_creation.py
@@ -6,73 +6,71 @@ from multiprocessing import Pool
 import os
 from os import listdir
 from os.path import isfile, join
-
-#we will ignore case and punctuation
+import gzip
+import time
 
 one_gram_counts = Counter()
 two_gram_counts = Counter()
 three_gram_counts = Counter()
 
-	
+
+
 def onegram(sentence):
-	text = re.sub('\W', ' ', sentence.lower())
+	text = re.sub("[^\w\d'\s]+",'',sentence)
 	words = text.split()
 	return words
 	
 	
 
+
 def bigrams(sentence):
-	text = re.sub('\W', ' ', sentence.lower())
+	text = re.sub("[^\w\d'\s]+",'',sentence)
 	words = text.split()
 	return zip(words, words[1:])
 	
 	
-	
-	
-	
+
+
 def trigrams(sentence):
-	text = re.sub('\W', ' ', sentence.lower())
+	text = re.sub("[^\w\d'\s]+",'',sentence)
 	words = text.split()
 	return zip(words, words[1:], words[2:])
 	
-	
-	
-	
-
-def process_gzip_file(gzip_file):
-	with gzip.open(gzip_file,'rt') as f:    
-		for line in f:
-			one_gram_counts.update(onegram(line))
-			two_gram_counts.update(bigrams(line))
-			three_gram_counts.update(trigrams(line))
 
 
-		
+#troubleshoot_counter = 0
+def process_gzip_file(gzip_file): #open each gzip file and count 1-gram, 2-gram, and 3-gram, might do 4 and 5-grams later
+    with gzip.open(gzip_file,'rt') as f:  
+        troubleshoot_counter = 0
+        for line in f:
+            #troubleshoot_counter += 1
+            #if troubleshoot_counter % 100000 == 0:
+                #print(troubleshoot_counter)
+            one_gram_counts.update(line)
+            two_gram_counts.update(bigrams(line))
+            three_gram_counts.update(trigrams(line))
+
+###### without multiprocess
+
+
+t1 = time.perf_counter()	
 if __name__ == "__main__":
-	path = 'test'
-	gzip_files = [f for f in listdir(path) if isfile(join(path, f))]
-		
-	num_processes = min(20, os.cpu_count())
-		
-	with Pool(processes = num_processes) as pool:
-		pool.map(process_gzip_file, gzip_files)
-			
+    path = 'Dolma/'
+    gzip_files = [(path + f) for f in listdir(path) if isfile(join(path, f))]	#all the gzip files in the directory
+    for i in gzip_files: #for each gzip file:
+        print('Current file: ', i)  #keep track of the current file
+        process_gzip_file(i)    #process the file
 
-	with open("one_gram_counts.txt", 'w') as f:
+t2 = time.perf_counter()
+t2 - t1
+
+### write each file into a txt file separated by tab
+with open("one_gram_counts.txt", 'w') as f:
 		for k,v in one_gram_counts.items():
-			f.write( "{}\t{}".format(k,v))
-				
-				
-	with open("one_gram_counts.txt", 'w') as f:
-		for k,v in two_gram_counts.items():
-			f.write( "{}\t{}".format(k,v))
-				
-				
-	with open("one_gram_counts.txt", 'w') as f:
-		for k,v in three_gram_counts.items():
-			f.write( "{}\t{}".format(k,v))
-			
-			
-			
-			
-			
+			f.write( "{}\t{}".format(k,v))			
+with open("two_gram_counts.txt", 'w') as f:
+    for k,v in two_gram_counts.items():
+        f.write( "{}\t{}".format(k,v))			
+with open("three_gram_counts.txt", 'w') as f:
+    for k,v in three_gram_counts.items():
+        f.write( "{}\t{}".format(k,v))
