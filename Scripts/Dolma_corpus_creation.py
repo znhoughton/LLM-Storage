def31217103ecb65ea0c6173ac41b0df1c36525e
znhoughton <znhoughton@gmail.com>
2024-06-01 08:28:57 -0700

    added parallel processing

--- Scripts/Dolma_corpus_creation.py
+++ Scripts/Dolma_corpus_creation.py
@@ -2,45 +2,34 @@ from collections import defaultdict
 from collections import Counter
 import json
 import re
-from multiprocessing import Pool
-import os
+import multiprocessing as mp
 from os import listdir
 from os.path import isfile, join
 import gzip
 import time
 
-one_gram_counts = Counter()
-two_gram_counts = Counter()
-three_gram_counts = Counter()
-
-
-
 def onegram(sentence):
 	text = re.sub("[^\w\d'\s]+",'',sentence)
 	words = text.split()
 	return words
-	
-	
-
 
 def bigrams(sentence):
 	text = re.sub("[^\w\d'\s]+",'',sentence)
 	words = text.split()
 	return zip(words, words[1:])
-	
-	
-
 
 def trigrams(sentence):
 	text = re.sub("[^\w\d'\s]+",'',sentence)
 	words = text.split()
 	return zip(words, words[1:], words[2:])
-	
 
-
+# This is for serial processing
+one_gram_counts = Counter()
+two_gram_counts = Counter()
+three_gram_counts = Counter()
 #troubleshoot_counter = 0
-def process_gzip_file(gzip_file): #open each gzip file and count 1-gram, 2-gram, and 3-gram, might do 4 and 5-grams later
-    with gzip.open(gzip_file,'rt') as f:  
+def process_gzip_file_serial(gzip_file): #open each gzip file and count 1-gram, 2-gram, and 3-gram, might do 4 and 5-grams later
+    with gzip.open(gzip_file,'rt', encoding='utf-8') as f:  
         troubleshoot_counter = 0
         for line in f:
             #troubleshoot_counter += 1
@@ -50,27 +39,59 @@ def process_gzip_file(gzip_file): #open each gzip file and count 1-gram, 2-gram,
             two_gram_counts.update(bigrams(line))
             three_gram_counts.update(trigrams(line))
 
-###### without multiprocess
+# this is for parallel processing
+pool_size = 50
 
+def process_individual_file(gzip_file):
+     one_gram_ind_counter = Counter()
+     two_gram_ind_counter = Counter()
+     three_gram_ind_counter= Counter()
+     with gzip.open(gzip_file,'rt', encoding='utf-8') as f:
+          for line in f:
+            one_gram_ind_counter.update(line)
+            two_gram_ind_counter.update(bigrams(line))
+            three_gram_ind_counter.update(trigrams(line))
+     return [one_gram_ind_counter, two_gram_ind_counter, three_gram_ind_counter]
+               
+def process_gzip_file_parallel(gzip_file):
+     pool = mp.Pool(pool_size)
+     results = pool.map(process_individual_file, [file for file in gzip_file])
+     print(results)
+     pool.close()
 
-t1 = time.perf_counter()	
-if __name__ == "__main__":
-    path = 'Dolma/'
-    gzip_files = [(path + f) for f in listdir(path) if isfile(join(path, f))]	#all the gzip files in the directory
-    for i in gzip_files: #for each gzip file:
-        print('Current file: ', i)  #keep track of the current file
-        process_gzip_file(i)    #process the file
-
-t2 = time.perf_counter()
-t2 - t1
+def process_results(result):
+     for file in result:
+         one_gram_counts.update(file[0])
+         two_gram_counts.update(file[1])
+         three_gram_counts.update(file[2])
 
 ### write each file into a txt file separated by tab
-with open("one_gram_counts.txt", 'w') as f:
-		for k,v in one_gram_counts.items():
-			f.write( "{}\t{}".format(k,v))			
-with open("two_gram_counts.txt", 'w') as f:
-    for k,v in two_gram_counts.items():
-        f.write( "{}\t{}".format(k,v))			
-with open("three_gram_counts.txt", 'w') as f:
-    for k,v in three_gram_counts.items():
-        f.write( "{}\t{}".format(k,v))
+def write_result_to_file():
+    with open("one_gram_counts.txt", 'w') as f:
+            for k,v in one_gram_counts.items():
+                f.write( "{}\t{}".format(k,v))			
+    with open("two_gram_counts.txt", 'w') as f:
+        for k,v in two_gram_counts.items():
+            f.write( "{}\t{}".format(k,v))			
+    with open("three_gram_counts.txt", 'w') as f:
+        for k,v in three_gram_counts.items():
+            f.write( "{}\t{}".format(k,v))
+
+def main():
+    if __name__ == "__main__":
+        t1 = time.perf_counter()
+
+        path = 'Dolma/'
+        gzip_files = [(path + f) for f in listdir(path) if isfile(join(path, f))]	#all the gzip files in the directory
+        results = process_gzip_file_parallel(gzip_files)
+        process_results(results)
+
+        write_result_to_file()
+
+        t2 = time.perf_counter()
+        print(t2 - t1)
+
+
+
+
+main()
